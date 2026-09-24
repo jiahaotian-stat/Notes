37002617fd74464d1640660025b3647e891d98a8
JHAO1122 <3230105412@zju.edu.cn>
2026-09-24 18:56:53 +0800

    Deployed 8c1b787d with MkDocs version: 1.6.1

--- javascripts/starry_map.js
+++ javascripts/starry_map.js
@@ -47,7 +47,6 @@ document.addEventListener("DOMContentLoaded", function() {
 
         { id: "Ad-Prob", parentId: "Prob-Stat", label: isEn ? "Advanced Probability" : "高等概率论", radius: 15, color: colors.probSub, collapsed: true },
 
-        { id: "DM", parentId: "Geometry", label: isEn ? "Differential Manifolds" : "微分流形与黎曼几何", radius: 15, color: colors.geometry, collapsed: true },
 
         { id: "Ad-Stat", parentId: "Prob-Stat", label: isEn ? "Advanced Mathematical Statistics" : "高等数理统计", radius: 15, color: colors.probSub, collapsed: true },
 
@@ -124,8 +123,6 @@ document.addEventListener("DOMContentLoaded", function() {
         { id: "Ad-Prob-Intro", label: isEn ? "0. Intro" : "课程简介", parentId: "Ad-Prob", radius: 8, color: colors.probSub, url: "Advanced_Prob/0Intro/" },
         //高等数理统计
         { id: "Ad-Stat-Intro", label: isEn ? "0. Intro" : "课程简介", parentId: "Ad-Stat", radius: 8, color: colors.probSub, url: "Advanced_Stat/0Intro/" },
-        //微分流形与黎曼几何
-        { id: "DM-Intro", label: isEn ? "0. Intro" : "课程简介", parentId: "DM", radius: 8, color: colors.geometry, url: "Differential_Manifolds/0Intro/" },
 
         //统计学习
         { id: "SL-Intro", label: isEn ? "0. Intro" : "课程简介", parentId: "SL", radius: 8, color: colors.probSub, url: "Statistical_Learning/0Intro/" },
@@ -157,7 +154,6 @@ document.addEventListener("DOMContentLoaded", function() {
         { source: "Analysis", target: "RA" },
         { source: "Prob-Stat", target: "SL" },
         { source: "Prob-Stat", target: "Ad-Prob" },
-        { source: "Geometry", target: "DM" },
         { source: "Prob-Stat", target: "Ad-Stat" },
 
 
@@ -180,7 +176,6 @@ document.addEventListener("DOMContentLoaded", function() {
         { source: "RA", target: "RA-Ch12" },
         { source: "Ad-Prob", target: "Ad-Prob-Intro" },
         { source: "Ad-Stat", target: "Ad-Stat-Intro" },
-        { source: "DM", target: "DM-Intro" },
 
         { source: "SL", target: "SL-Intro" }, { source: "SL", target: "SL-Ch1" }, { source: "SL", target: "SL-Ch2" }, { source: "SL", target: "SL-Ch3" },
         { source: "SL", target: "SL-Ch4" }, { source: "SL", target: "SL-Ch5" }, { source: "SL", target: "SL-Ch6" }, { source: "SL", target: "SL-Ch7" },
